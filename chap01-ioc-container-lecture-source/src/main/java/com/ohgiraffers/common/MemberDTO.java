7d53deedb718e475e4cf4f0b5474dec6c5249098
DESKTOP-GYUNAM <kimkunam06@gmail.com>
2024-11-06 10:22:36 +0900

    🔧 [24-11-06 수] 1교시 spring core -  (xml 방식으로 컨테이너 제작)

---
+++ chap01-ioc-container-lecture-source/src/main/java/com/ohgiraffers/common/MemberDTO.java
@@ -0,0 +1,13 @@
+package com.ohgiraffers.common;
+
+// 24-11-06 (수) 스프링, DTO 구성
+public class MemberDTO {
+
+    private int no;
+    private String id;
+    private String pwd;
+    private String name;
+
+    public MemberDTO(){}
+
+}
---
+++ chap01-ioc-container-lecture-source/src/main/java/com/ohgiraffers/section01/xmlconfig/Application.java
@@ -0,0 +1,11 @@
+package com.ohgiraffers.section01.xmlconfig;
+
+// 24-11-06 (수) 1교시 스프링 메인 클래스
+public class Application {
+
+    public static void main(String[] args) {
+
+        /* index. 1. xml 방식으로 컨테이너 구성 */
+
+    }
+}
